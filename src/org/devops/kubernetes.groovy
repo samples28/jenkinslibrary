16424ea06da711280bb95179168fefdb1a5524eb
samples28 <ct_student@163.com>
2021-02-25 15:53:58 +0800

    Update kubernetes.groovy

--- src/org/devops/kubernetes.groovy
+++ src/org/devops/kubernetes.groovy
@@ -40,7 +40,7 @@ def CreateDeployment(nameSpace,deployName,deplyBody){
     apiUrl = "namespaces/${nameSpace}/deployments/"
     response = HttpReq('POST',apiUrl,deplyBody)
     def dresponse = readYaml text: "${response.content}"
-    comid = dresponse['kind']
+    comid = dresponse['metadata']['name']
     println(comid)
     //println("Content: "+response.content)
 
